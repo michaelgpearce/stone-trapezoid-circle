290adbcf9f01f9bdee7107a45c5d1cf7ac5fb236
Michael Pearce <michaelgpearce@yahoo.com>
2021-05-24 23:04:13 -0700

    Add 2 cut trapezoid

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,8 @@
 function draw() {
   const pi = Math.PI
   const form = document.getElementById('form')
-  const kerf = parseFloat(form.kerf.value)
+  const cutCount = form.cutCount.value == "1" ? 1 : 2
+  const kerf = cutCount == 1 ? parseFloat(form.kerf.value) : 0
   const length = parseFloat(form.length.value)
   const depth = parseFloat(form.depth.value)
   const diameter = parseFloat(form.diameter.value) * 12.0
@@ -13,8 +14,8 @@ function draw() {
 
   const ratio = innerCircumference / outerCircumference
 
-  const longLength = (length - kerf) / (1 + ratio)
-  const shortLength = (length - kerf) - longLength
+  const longLength = cutCount == 1 ? ((length - kerf) / (1 + ratio)) : (length * 0.5)
+  const shortLength = cutCount == 1 ? ((length - kerf) - longLength) : (length * ratio * 0.5)
   const anglePerStone = calculateAnglePerStone(longLength, shortLength, depth)
 
   const stoneCount = Math.floor(360.0 / anglePerStone) - removeStones
@@ -38,6 +39,7 @@ function draw() {
       shortLength,
       depth,
       radius,
+      cutCount == 1,
     )
   })
   const stonesSvg = [
@@ -64,6 +66,7 @@ function draw() {
     buildLineSvg(
       [longLength, 0],
       [shortLength, depth],
+      true,
     ),
     '</svg>'
   ].join('')
@@ -99,6 +102,20 @@ function draw() {
     ),
     '</svg>'
   ].join('')
+
+  document.getElementById('fullSideLengthsStone').innerHTML = [
+    `<svg style="background-color: #3D40FC; width: 100%; max-height: 100%" viewBox="0 0 ${longLength * 2.0} ${depth}" preserveAspectRatio="none">`,
+    buildTrapezoidSvg(
+      [0, 0],
+      [longLength * 2.0, 0],
+      [longLength + shortLength, depth],
+      [longLength - shortLength, depth],
+    ),
+    '</svg>'
+  ].join('')
+
+  $('.cutCountContainer').hide()
+  $(`.cutCountContainer.cutCount${cutCount}`).show()
 }
 
 function calculateStoneRotation(stoneCount, anglePerStone) {
@@ -119,7 +136,7 @@ function buildCircleSvg(radius) {
   return `<circle cx="0" cy="0" r="${radius}" style="${style}" />`
 }
 
-function buildStoneSvg(stoneIndex, longLength, shortLength, depth, radius) {
+function buildStoneSvg(stoneIndex, longLength, shortLength, depth, radius, showCut) {
   const rotation = stoneIndex * calculateAnglePerStone(longLength, shortLength, depth)
 
   return [
@@ -130,17 +147,21 @@ function buildStoneSvg(stoneIndex, longLength, shortLength, depth, radius) {
       [shortLength, -radius + depth],
       [-shortLength, -radius + depth],
     ),
-    buildLineSvg(
-      [0, -radius],
-      [0, -radius + depth],
-    ),
+    showCut ?
+      buildLineSvg(
+        [0, -radius],
+        [0, -radius + depth],
+      ) :
+      '',
     '</g>'
   ].join('')
 }
 
-function buildLineSvg(point1, point2) {
+function buildLineSvg(point1, point2, dashed) {
+  const dashedStyle = dashed ? 'stroke-dasharray:1' : ''
+
   return [
-    `<line x1="${point1[0]}" y1="${point1[1]}" x2="${point2[0]}" y2="${point2[1]}" style="stroke:black; stroke-width:0.1; stroke-dasharray:1" />`
+    `<line x1="${point1[0]}" y1="${point1[1]}" x2="${point2[0]}" y2="${point2[1]}" style="stroke:black; stroke-width:0.1; ${dashedStyle}" />`
   ].join('')
 }
 
@@ -175,17 +196,20 @@ function updateStoneInfo(longLength, shortLength, kerf) {
   document.getElementById('shortCutWithKerf').innerHTML = shortLength.toFixed(3)
   document.getElementById('longSide').innerHTML = (longLength * 2.0).toFixed(3)
   document.getElementById('shortSide').innerHTML = (shortLength * 2.0).toFixed(3)
+  document.getElementById('fullLongSide').innerHTML = (longLength * 2.0).toFixed(3)
+  document.getElementById('fullShortSide').innerHTML = (shortLength * 2.0).toFixed(3)
+  document.getElementById('cutFromEdge').innerHTML = (longLength - shortLength).toFixed(3)
 }
 
 function syncFormToParameters() {
   const form = document.getElementById('form')
   const searchParams = new URLSearchParams(window.location.search)
-  const params = ['kerf', 'length', 'depth', 'diameter', 'removeStones']
+  const params = ['cutCount', 'kerf', 'length', 'depth', 'diameter', 'removeStones']
 
   params.forEach((param) => {
     const value = searchParams.get(param)
     if (value) {
-      form[param].value = value
+      $(form[param]).val(value)
     }
   })
 }
